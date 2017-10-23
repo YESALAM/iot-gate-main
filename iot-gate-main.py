1cb7ffe6b0bf9addb184c1a76f60d25206a8a93a
yesalam <itisalam@gmail.com>
2017-10-23 22:44:22 +0530

    real read function

--- iot-gate-main.py
+++ iot-gate-main.py
@@ -9,6 +9,62 @@ from wtforms import validators
 
 from time import sleep
 
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+
+continue_reading = True
+
+# Capture SIGINT for cleanup when the script is aborted
+def end_read():
+    global continue_reading
+    #print "Ctrl+C captured, ending read."
+    continue_reading = False
+    GPIO.cleanup()
+
+def readyToread():
+    # Create an object of the class MFRC522
+    MIFAREReader = MFRC522.MFRC522()
+    # This loop keeps checking for chips. If one is near it will get the UID and authenticate
+    while continue_reading:
+
+        # Scan for cards
+        (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+        # If a card is found
+        if status == MIFAREReader.MI_OK:
+            #print "Card detected"
+
+        # Get the UID of the card
+        (status, uid) = MIFAREReader.MFRC522_Anticoll()
+
+        # If we have the UID, continue
+        if status == MIFAREReader.MI_OK:
+
+            # Print UID
+            #print "Card read UID: " + str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3])
+            uid = str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3])
+            # This is the default key for authentication
+            key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
+            # Select the scanned tag
+            MIFAREReader.MFRC522_SelectTag(uid)
+
+            # Authenticate
+            status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+            result = "";
+            # Check if authenticated
+            if status == MIFAREReader.MI_OK:
+                MIFAREReader.MFRC522_Read(8)
+                MIFAREReader.MFRC522_StopCrypto1()
+                result = "{status:'ok',result:'"+uid+"'}"
+                end_read()
+            else:
+                #print "Authentication error"
+                result = "{status:'not authenticated'}"
+                end_read()
+    return result
 
 # App config.
 DEBUG = True
@@ -48,8 +104,8 @@ def hello_world():
 
 @app.route('/readtag',methods=['GET'])
 def readTag():
-    sleep(20)
-    return '{result:"verygood"}'
+    #sleep(20)
+    #return '{result:"verygood"}'
     return readyToread()
 
 if __name__ == '__main__':
